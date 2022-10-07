cbdfd76b175f90fe3364e988ceff2933879a6b49
bemail <115222072+bemail@users.noreply.github.com>
2022-10-07 21:21:44 +0700

    Update index.js

--- index.js
+++ index.js
@@ -15,7 +15,7 @@ const emailForm = document.querySelector("#emailForm");
 
 const toast = document.querySelector(".toast");
 
-const baseURL = "https://innshare.herokuapp.com";
+const baseURL = "https://bemail.github.io/inshare/";
 const uploadURL = `${baseURL}/api/files`;
 const emailURL = `${baseURL}/api/files/send`;
 
